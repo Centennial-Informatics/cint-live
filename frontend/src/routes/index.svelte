a5cdb4be9866fa3ce44e3a5f26631709e5dd74cd
jtcheng26 <jtcheng226@gmail.com>
2022-05-12 21:54:43 -0400

    Make website issues link more visible

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -56,17 +56,22 @@
 	<Page>
 		{#if $contestData.Info}
 			<Header>
+				<a
+					class="text-xl font-bold text-white rounded-lg p-3 bg-alt-darkest hover:bg-alt-dark"
+					href="https://docs.google.com/document/d/1JTN0ENdYsaMEsUhO96iPPwOQWm6ZQQ6IsmW4sdvperM/edit"
+				>
+					Note all website issues <a
+						class="text-alt-light hover:underline"
+						href="https://docs.google.com/document/d/1JTN0ENdYsaMEsUhO96iPPwOQWm6ZQQ6IsmW4sdvperM/edit"
+						>Here</a
+					>
+				</a>
 				<Title>{$contestInfo.Name}</Title>
 				<Subtitle>{dateString}</Subtitle>
 				<Subtitle>{startTimeString} – {stopTimeString}</Subtitle>
 				<Subtitle>{location}</Subtitle>
 			</Header>
-			<Paragraph
-				>Note all website issues here <a
-					href="https://docs.google.com/document/d/1JTN0ENdYsaMEsUhO96iPPwOQWm6ZQQ6IsmW4sdvperM/edit"
-					>https://docs.google.com/document/d/1JTN0ENdYsaMEsUhO96iPPwOQWm6ZQQ6IsmW4sdvperM/edit</a
-				></Paragraph
-			>
+
 			<Paragraph>{$contestInfo.Description}</Paragraph>
 			<Paragraph>
 				Teams may have up to four participants. Each team member’s submission will score for the
