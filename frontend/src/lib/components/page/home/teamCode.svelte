8d6408df0169576bb5d6af8d7a623a582bb5d804
jtcheng26 <jtcheng226@gmail.com>
2022-05-12 21:32:30 -0400

    Add copy to clipboard to sample tests and team code

--- frontend/src/lib/components/page/home/teamCode.svelte
+++ frontend/src/lib/components/page/home/teamCode.svelte
@@ -2,12 +2,29 @@
 	import { brand } from '$lib/data/stores/userInfo';
 
 	export let teamCode: string;
+	let copied = false;
+	let copyErr = '';
+
+	async function copyCodeToClipboard() {
+		try {
+			await navigator.clipboard.writeText(teamCode);
+			copied = true;
+		} catch (err) {
+			copyErr = 'Failed to copy: ' + err;
+		}
+	}
 </script>
 
 <div
 	class="bg-{$brand} w-full p-6 rounded-xl flex flex-row text-md text-gray-50 items-center hover:bg-opacity-70 cursor-pointer"
+	on:click={copyCodeToClipboard}
 >
 	Your Team Code
+	{#if copied}
+		<span class="ml-2 font-bold">Copied to clipboard!</span>
+	{:else if copyErr}
+		<span class="ml-2 font-bold">{copyErr}</span>
+	{/if}
 	<div class="flex-grow" />
 	<div class="text-4xl font-bold">{teamCode.toUpperCase()}</div>
 </div>
--- frontend/src/lib/components/page/problem/sampleTest.svelte
+++ frontend/src/lib/components/page/problem/sampleTest.svelte
@@ -1,17 +1,55 @@
 <script lang="ts">
 	import Code from '$lib/components/templates/typography/code.svelte';
+	import { brand } from '$lib/data/stores/userInfo';
 
 	export let input = '';
 	export let output = '';
+
+	let inputCopied = false;
+	let outputCopied = false;
+
+	async function copyInputToClipboard() {
+		try {
+			await navigator.clipboard.writeText(input);
+			inputCopied = true;
+		} catch (err) {
+			console.log('Failed to copy input: ' + err);
+		}
+	}
+
+	async function copyOutputToClipboard() {
+		try {
+			await navigator.clipboard.writeText(output);
+			outputCopied = true;
+		} catch (err) {
+			console.log('Failed to copy output: ' + err);
+		}
+	}
 </script>
 
 <div class="flex flex-row w-full space-x-4 pt-1">
-	<div class="w-1/2 text-gray-400 bg-gray-600 p-3 rounded-lg shadow-lg">
-		Input
+	<div
+		class="w-1/2 text-gray-400 bg-gray-600 p-3 rounded-lg shadow-lg hover:bg-opacity-70 cursor-pointer"
+		on:click={copyInputToClipboard}
+	>
+		<div class="flex flex-row justify-between">
+			Input
+			{#if inputCopied}
+				<span class="font-bold text-{$brand}-light">Copied!</span>
+			{/if}
+		</div>
 		<Code code={input} />
 	</div>
-	<div class="w-1/2 text-gray-400 bg-gray-600 p-3 rounded-lg shadow-lg">
-		Output
+	<div
+		class="w-1/2 text-gray-400 bg-gray-600 p-3 rounded-lg shadow-lg hover:bg-opacity-70 cursor-pointer"
+		on:click={copyOutputToClipboard}
+	>
+		<div class="flex flex-row justify-between">
+			Output
+			{#if outputCopied}
+				<span class="font-bold text-{$brand}-light">Copied!</span>
+			{/if}
+		</div>
 		<Code code={output} />
 	</div>
 </div>
