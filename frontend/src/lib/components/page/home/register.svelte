e71410bfb9d7450b567853b2321afffd2ce99ece
jtcheng26 <jtcheng226@gmail.com>
2022-05-12 22:10:58 -0400

    Update registration text

--- frontend/src/lib/components/page/home/register.svelte
+++ frontend/src/lib/components/page/home/register.svelte
@@ -4,12 +4,19 @@
 </script>
 
 <Wrapper margin={false} padding={false}>
-	<div class="py-8 space-y-4 flex flex-col justify-center items-center">
+	<div class="py-8 space-y-4 flex flex-col justify-center items-center px-4">
 		<div class="text-white text-xl p-4">
-			<strong>If you haven't reigistered:</strong>
+			<strong>If you haven't registered:</strong>
 			<div>1. Click the <strong>Register</strong> button down below.</div>
-			<div>2. Enter your <strong>SCHOOL EMAIL</strong> and additional information.</div>
-			<div>3. Reload this page and sign in through the popup using your school email.</div>
+			<div>
+				2. Enter your preferred <strong>Google Email</strong> (can be a school account) and additional
+				information.
+			</div>
+			<div>
+				3. Reload this page and sign in through the popup or the sign-in button <strong
+					>using the email you registered with</strong
+				>.
+			</div>
 		</div>
 		<a
 			href={REGISTRATION_FORM_LINK}
