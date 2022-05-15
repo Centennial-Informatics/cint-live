8e374c0b1dc17d90b5fe5564e49820aa99faeeb8
jtcheng26 <jtcheng226@gmail.com>
2022-05-15 15:12:59 -0400

    Fix cell overflow scrolling

--- frontend/src/lib/components/page/standings/standingsCell.svelte
+++ frontend/src/lib/components/page/standings/standingsCell.svelte
@@ -3,6 +3,6 @@
 	export let left = false;
 </script>
 
-<div class="inline-block w-12 overflow-x-visible text-{left ? 'left' : 'center'} {className}">
+<div class="inline-block w-12 text-{left ? 'left' : 'center'} {className}">
 	<slot />
 </div>
